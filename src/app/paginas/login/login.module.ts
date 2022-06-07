f23391b770d2cf20ce7fed28087626a9dea0e295
sergio-asenjo <sergio.asenjo@usach.cl>
2022-06-07 15:14:36 -0400

    feat(login): implementa login con lógica.
    Se añade diseño de login con sus respectivos componentes para poder
    ingresar a cada página.
    
    Se añade lógica que deriva a cada usuario a su respectiva sesión, ya sea
    profesor o sea un apoderado.
    
    Se añaden usuarios dummies para apoderados y profesores.

--- src/app/paginas/login/login.module.ts
+++ src/app/paginas/login/login.module.ts
@@ -1,7 +1,7 @@
 import { FormularioComponent } from './../../componentes/formulario/formulario.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -14,7 +14,8 @@ import { LoginPage } from './login.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [LoginPage, FormularioComponent]
 })
