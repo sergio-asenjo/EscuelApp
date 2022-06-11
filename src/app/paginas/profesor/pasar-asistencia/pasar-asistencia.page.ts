a2fefd90c2f2578dc43f10b2c34c7b94d5d04085
sergio-asenjo <sergio.asenjo@usach.cl>
2022-06-11 02:09:27 -0400

    feat: añade páginas para profesor.
    Se añade página profesor/pasar-asistencia para que se permita que cada
    profesor pueda tomar asistencia directamente desde la app por cada
    asignatura que tenga registrada.
    
    Se añade página profesor/subir-notas para que el profesor pueda subir
    notas por asignatura a cada alumno, contando con funcionalidades de
    determinar un puntaje máximo y un porcentaje de exigencia para que la
    nota sea calculada automáticamente basada en dichos criterios.

--- src/app/paginas/profesor/pasar-asistencia/pasar-asistencia.page.ts
+++ src/app/paginas/profesor/pasar-asistencia/pasar-asistencia.page.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pasar-asistencia',
   templateUrl: './pasar-asistencia.page.html',
   styleUrls: ['./pasar-asistencia.page.scss'],
 })
-export class PasarAsistenciaPage implements OnInit {
+export class PasarAsistenciaPage {
+  public listadoAlumnos: Alumno[] = [
+    { nombre: 'Maximino Yañez', esAsistio: false },
+    { nombre: 'Ane Patiño', esAsistio: false },
+    { nombre: 'Luis Carlos Raya', esAsistio: false },
+    { nombre: 'Enzo Matas', esAsistio: false },
+    { nombre: 'Monica Martínez', esAsistio: false },
+    { nombre: 'Remedios Ubeda', esAsistio: false },
+    { nombre: 'Latifa Casado', esAsistio: false },
+    { nombre: 'Yeray Aznar', esAsistio: false },
+    { nombre: 'Marwa Juan', esAsistio: false },
+    { nombre: 'Sabina Ojeda', esAsistio: false },
+    { nombre: 'Carles Calvo', esAsistio: false },
+    { nombre: 'Antonia Aguirre', esAsistio: false },
+  ];
+  public esMostrarAlumnos: boolean = false;
+  public clase: string = '';
+  public fechaHoy: Date = new Date();
 
-  constructor() { }
+  constructor(private loader: LoadingController) { }
 
-  ngOnInit() {
+  mostrarAlumnos(clase: string) {
+    this.esMostrarAlumnos = true;
+    this.clase = clase;
   }
 
+  async registrarAsistencia() {
+    await this.mostrarCargaAsistencia();
+    await new Promise((r) => setTimeout(r, 3000));
+    await this.mostrarMensajeFinalizacion();
+    await new Promise((r) => setTimeout(r, 3000));
+    this.esMostrarAlumnos = false;
+    this.limpiarAsistencia();
+  }
+
+  private async mostrarCargaAsistencia() {
+    const loading = await this.loader.create({
+      cssClass: 'loader-asistencia',
+      message: 'Registrando Asistencia...',
+      mode: 'ios',
+      duration: 3000,
+      spinner: 'crescent',
+      backdropDismiss: false,
+    });
+    await loading.present();
+  }
+
+  private async mostrarMensajeFinalizacion() {
+    const loading = await this.loader.create({
+      cssClass: 'mensaje-exito',
+      message: '¡Asistencia registrada exitosamente!',
+      mode: 'ios',
+      duration: 3000,
+      backdropDismiss: false,
+      spinner: null,
+    });
+    await loading.present();
+  }
+
+  private limpiarAsistencia() {
+    this.listadoAlumnos.forEach((alumno) => {
+      alumno.esAsistio = false;
+    });
+  }
+}
+
+interface Alumno {
+  nombre: string;
+  esAsistio: boolean;
 }
--- src/app/paginas/profesor/subir-notas/subir-notas.page.ts
+++ src/app/paginas/profesor/subir-notas/subir-notas.page.ts
@@ -1,15 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-subir-notas',
   templateUrl: './subir-notas.page.html',
   styleUrls: ['./subir-notas.page.scss'],
 })
-export class SubirNotasPage implements OnInit {
+export class SubirNotasPage {
+  public listadoAlumnos: Alumno[] = [
+    { nombre: 'Maximino Yañez', puntaje: 0},
+    { nombre: 'Ane Patiño', puntaje: 0},
+    { nombre: 'Luis Carlos Raya', puntaje: 0},
+    { nombre: 'Enzo Matas', puntaje: 0},
+    { nombre: 'Monica Martínez', puntaje: 0},
+    { nombre: 'Remedios Ubeda', puntaje: 0},
+    { nombre: 'Latifa Casado', puntaje: 0},
+    { nombre: 'Yeray Aznar', puntaje: 0},
+    { nombre: 'Marwa Juan', puntaje: 0},
+    { nombre: 'Sabina Ojeda', puntaje: 0},
+    { nombre: 'Carles Calvo', puntaje: 0},
+    { nombre: 'Antonia Aguirre', puntaje: 0},
+  ];
+  public esMostrarAlumnos: boolean = false;
+  public puntajeMaximo: number = 150;
+  public porcentajeExigencia: number = 60;
+  public clase: string = '';
+  public fechaHoy: Date = new Date();
 
-  constructor() { }
+  constructor(private loader: LoadingController) { }
 
-  ngOnInit() {
+  mostrarAlumnos(clase: string) {
+    this.esMostrarAlumnos = true;
+    this.clase = clase;
   }
 
+  calcularNota(puntaje: number) {
+    const exigencia = this.porcentajeExigencia / 100;
+    if (puntaje < exigencia * this.puntajeMaximo) {
+      return ((3 * (puntaje / (exigencia * this.puntajeMaximo))) + 1);
+    } else if (puntaje >= exigencia * this.puntajeMaximo) {
+      return ((3 * ((puntaje - (exigencia * this.puntajeMaximo)) / (this.puntajeMaximo * (1 - exigencia)))) + 4);
+    }
+  }
+
+  async guardarNotas() {
+    await this.mostrarCargaNotas();
+    await new Promise((r) => setTimeout(r, 3000));
+    await this.mostrarMensajeFinalizacion();
+    await new Promise((r) => setTimeout(r, 3000));
+    this.esMostrarAlumnos = false;
+    this.reiniciarData();
+  }
+
+  private async mostrarCargaNotas() {
+    const loading = await this.loader.create({
+      cssClass: 'loader-asistencia',
+      message: 'Guardando Notas...',
+      mode: 'ios',
+      duration: 3000,
+      spinner: 'crescent',
+      backdropDismiss: false,
+    });
+    await loading.present();
+  }
+
+  private async mostrarMensajeFinalizacion() {
+    const loading = await this.loader.create({
+      cssClass: 'mensaje-exito',
+      message: '¡Notas registradas exitosamente!',
+      mode: 'ios',
+      duration: 3000,
+      backdropDismiss: false,
+      spinner: null,
+    });
+    await loading.present();
+  }
+
+  private reiniciarData() {
+    this.puntajeMaximo = 150;
+    this.porcentajeExigencia = 60;
+    this.listadoAlumnos.forEach((alumno) => {
+      alumno.puntaje = 0;
+    });
+  }
+}
+
+interface Alumno {
+  nombre: string;
+  puntaje: number;
 }
